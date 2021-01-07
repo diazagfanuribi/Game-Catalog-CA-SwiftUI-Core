1d1c50dad443ee5328395357fad22f93c44c1ded
diazagfanuribi <diazagfa@gmail.com>
2021-01-07 12:16:25 +0700

    Initial commit

---
+++ Sources/Core/Mapper.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by DiazAgfa on 04/01/21.
+//
+
+import Foundation
+ 
+
+public protocol Mapper {
+    associatedtype Request
+    associatedtype Response
+    associatedtype Entity
+    associatedtype Domain
+    
+    func transformResponseToEntity(request: Request?, response: Response) -> Entity
+    func transformEntityToDomain(entity: Entity) -> Domain
+}
